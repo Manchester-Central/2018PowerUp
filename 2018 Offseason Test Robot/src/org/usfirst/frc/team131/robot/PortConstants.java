4dc0184d3f9ab20113b7d9ab2978dba3c17349fd
ashnh <ashnhx@gmail.com>
2018-01-13 13:51:09 -0500

    Added test code for raft robot

---
+++ 2018 Offseason Test Robot/src/org/usfirst/frc/team131/robot/PortConstants.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team131.robot;
+
+public interface PortConstants {
+
+	
+	int LEFT_BACK_TALON = 0;
+	int LEFT_MID_TALON = 1;
+	int LEFT_FRONT_TALON = 2;
+	
+	int RIGHT_BACK_TALON = 3;
+	int RIGHT_MID_TALON = 4;
+	int RIGHT_FRONT_TALON = 5;
+	
+	int LEFT_CAN_TALON = 0;
+	int RIGHT_CAN_TALON = 1;
+	
+	
+	int GEAR_SHIFTER_A_SLOW = 5;
+	int GEAR_SHIFTER_B_SLOW = 6;	
+	
+	
+}
