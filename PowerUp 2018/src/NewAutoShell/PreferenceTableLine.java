1f21536fdc3ac86d5a83100862943d339d2ffcf8
ashnh <ashnhx@gmail.com>
2018-01-20 15:42:05 -0500

    Eason added UnitTest to eclipse workspaces

--- PowerUp 2018/src/NewAutoShell/PreferenceTableLine.java
+++ PowerUp 2018/src/NewAutoShell/PreferenceTableLine.java
@@ -41,5 +41,7 @@ public class PreferenceTableLine {
 	public boolean getIsParallel () {
 		return isParallel;
 	}
+	
+	
 
 }
---
+++ PowerUp 2018/src/NewAutoShell/PreferenceTableLineTest.java
@@ -0,0 +1,45 @@
+package NewAutoShell;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+class PreferenceTableLineTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	
+		
+		assertEquals(match("dsfds"), false);
+		assertEquals(match("test;dsfds."), true );
+		assertEquals(match(""), false);
+		assertEquals(match("jeff4&12,"), false);
+		assertEquals(match("test;yes."), true);
+		
+		assertEquals(match("turn;87&-87."), true);
+//		match("");
+//		match("turn87.");
+//		match("turn;87;87.");
+//		match("turn;87;87,");
+//		match("turn;87");
+
+	}
+
+	
+	public boolean match(String check) {
+		CommandGroup autoSequence = new CommandGroup();
+		Map<String, ChaosCommand> commands = new HashMap<String, ChaosCommand>();
+		commands.put("test", new TestChaosCommand());
+		PreferenceTableLine line = new PreferenceTableLine(check, commands);
+		System.out.print(line.getCommand() != null);
+		System.out.println(line.getIsParallel());
+		return line.getCommand() != null;
+		
+	}
+}
